8f5717dd4f24d4065b560e29217839e125b05442
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-21 10:00:15 +0530

    Update main.tf

--- Infra/modules/lambda/main.tf
+++ Infra/modules/lambda/main.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "add_sample_data" {
   function_name = "m1-add-sample-data"
   runtime       = "python3.10"
   role          = var.lambda_execution_role_arn
-  handler       = "m1-add-sample-data.lambda_handler"
+  handler       = "add_user.lambda_handler"
 
   filename      = var.add_user_zip_file
 
@@ -19,7 +19,7 @@ resource "aws_lambda_function" "get_users" {
   function_name = "get-users"
   runtime       = "python3.10"
   role          = var.lambda_execution_role_arn
-  handler       = "get-users.lambda_handler"
+  handler       = "get_user.lambda_handler"
 
   filename      = var.get_user_zip_file
 
