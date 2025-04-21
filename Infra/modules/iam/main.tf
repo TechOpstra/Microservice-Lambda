d92085781bf499e95cd0777c6cb78b1db766c65c
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-21 09:26:59 +0530

    Update main.tf

--- Infra/modules/iam/main.tf
+++ Infra/modules/iam/main.tf
@@ -1,5 +1,3 @@
-# modules/iam/main.tf
-
 resource "aws_iam_role" "lambda_execution_role" {
   name = var.role_name
 
@@ -18,13 +16,21 @@ resource "aws_iam_role" "lambda_execution_role" {
   })
 }
 
+# Attach DynamoDB Full Access policy
 resource "aws_iam_role_policy_attachment" "lambda_dynamodb_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
   role       = aws_iam_role.lambda_execution_role.name
 }
 
+# Attach DynamoDB Read-Only Access policy
 resource "aws_iam_role_policy_attachment" "lambda_dynamodb_readonly_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBReadOnlyAccess"
+  role       = aws_iam_role.lambda_execution_role.name
+}
+
+# Attach Lambda Basic Execution policy for CloudWatch Logs
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution_role" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda_execution_role.name
 }
 
