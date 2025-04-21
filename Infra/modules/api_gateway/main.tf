c156460418b14aa3acf188c65e1d4dead8c4e57c
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-21 10:19:09 +0530

    Update main.tf

--- Infra/modules/api_gateway/main.tf
+++ Infra/modules/api_gateway/main.tf
@@ -35,6 +35,10 @@ resource "aws_api_gateway_stage" "serverless_api_stage" {
   stage_name    = "v1"
   rest_api_id   = aws_api_gateway_rest_api.serverless_rest_api.id
   deployment_id = aws_api_gateway_deployment.serverless_api_deployment.id
+
+  logging_level = "INFO"  # Enable logging level
+  data_trace_enabled = true  # Optional, enables full request/response logging
+  cloudwatch_role_arn = aws_iam_role.api_gateway_cloudwatch_logs.arn
 }
 
 # Deploy the API
